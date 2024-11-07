1a6d4eb9721a178a5fb4ce499a837822d375425f
yantosan502 <144647630+yantosan502@users.noreply.github.com>
2024-11-07 19:31:37 +0700

    tampilan home

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Praktikum Basis Data </title>
+    <!-- Load file CSS Bootstrap offline -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+  <a class="navbar-brand" href="#">Selamat Datang!</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-auto">
+      <a class="nav-link active" href="#">Home</a>
+      <a class="nav-link" href="barang">Barang</a>
+      <a class="nav-link" href="pelanggan">Pelanggan</a>
+      <a class="nav-link" href="supplier">Supplier</a>
+      <a class="nav-link" href="penjualan">Penjualan</a>
+    </div>
+  </div>
+    </div>
+</nav>
+<!-- akhir navbar -->
+<!-- ini jumbotron -->
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Ardiyanto Satrio Nugroho</h1>
+    <p class="lead">"20108022"</p>
+    <p class="lead">Basis Data</p>
+  </div>
+</div>
+<!-- ahir jumbotron -->
+<script src="bootstrap/js/jquery-3.5.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
+</body>
+</html>
